8ebe5a5434d7d6aa22cd68ff741679dc2827db60
Sherwin Okhowat <okthatssherwin@gmail.com>
2025-10-23 17:26:47 -0400

    nnunet baseline script

--- docker/app/main.py
+++ docker/app/main.py
@@ -4,15 +4,18 @@ Write your app here.
 The folder where all datasets are stored is "/workspace/input", and it is read-only.
 The folder where you need to store your output is "/workspace/output", and it is writable.
 """
+
 from time import sleep
 
 if __name__ == "__main__":
-    f = open("/workspace/output/progress.txt", "w")
-    print("started")
-    f.write("started")
-    for i in range(120):
-        sleep(1)
-        print(i)
-        f.write(str(i))
-    print("started")
-    f.write("completed")
+    sleep(1)
+    pass
+    # f = open("/workspace/output/progress.txt", "w")
+    # print("started")
+    # f.write("started")
+    # for i in range(120):
+    #     sleep(1)
+    #     print(i)
+    #     f.write(str(i))
+    # print("started")
+    # f.write("completed")
--- docker/app/main.sh
+++ docker/app/main.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-python3.12 main.py
+exec > >(tee -a /workspace/output/progress.txt) 2>&1
+set -e
+
+export nnUNet_raw="/workspace/input/nnUNet_raw"
+export nnUNet_preprocessed="/workspace/output/nnUNet_preprocessed"
+export nnUNet_results="/workspace/output/nnUNet_weights"
+
+echo "[INFO] Starting nnU-Net AbdomenCT-1K baseline training..."
+date
+
+nnUNetv2_plan_and_preprocess -d 101 -c "3d_fullres" --verify_dataset_integrity
+
+for fold in 0 1 2 3 4; do
+    echo "[INFO] Training fold $fold..."
+    nnUNetv2_train "Dataset101_AbdomenCT1K" "3d_fullres" $fold --npz
+done
+
+echo "[INFO] Training complete."
+date
+
+echo "[INFO] Aggregating k fold outputs."
+nnUNetv2_find_best_configuration 101 -c "3d_fullres"
+
+# echo "[INFO] Training complete. Running post-job script..."
+# python3.12 main.py - we dont have to run main.py for now
+echo "[INFO] Done."
