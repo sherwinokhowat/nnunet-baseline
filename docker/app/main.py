b855272b1b5ea3608917b607610ac87a4e5e289d
Sherwin Okhowat <okthatssherwin@gmail.com>
2025-10-30 01:19:32 -0400

    changed epochs to 200

--- docker/app/main.py
+++ docker/app/main.py
@@ -10,12 +10,3 @@ from time import sleep
 if __name__ == "__main__":
     sleep(1)
     pass
-    # f = open("/workspace/output/progress.txt", "w")
-    # print("started")
-    # f.write("started")
-    # for i in range(120):
-    #     sleep(1)
-    #     print(i)
-    #     f.write(str(i))
-    # print("started")
-    # f.write("completed")
